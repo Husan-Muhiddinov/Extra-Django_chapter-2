cab799c10fcc4ebc4b254329f38ba5e562eca077
Husan-Muhiddinov <124786003+Husan-Muhiddinov@users.noreply.github.com>
2024-02-07 15:04:58 +0500

    Extra-Django AC-course

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,33 +1,28 @@
-import json
-from datetime import datetime
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import ChatGroup, Message
+from datetime import datetime
+import json
+from .models import ChatGroup, Message, Profile
 from django.contrib.auth.models import User
 from channels.db import database_sync_to_async
 
-
 class ChatConsumer(AsyncWebsocketConsumer):
+    
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["name"]
 
-        await self .channel_layer.group_add(self.room_name, self.channel_name)
+        await self.channel_layer.group_add(self.room_name, self.channel_name)
         await self.accept()
-        await self.channel_layer.group_send(self.room_name, {"type":"fetch_history"})
-
-
+        await self.fetch_history()
 
     async def disconnect(self, close_code):
-        await self .channel_layer.group_discard(self.room_name, self.channel_name)
-
-
-
+        await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        time = await self.create_messages(message=text_data_json['message'], sender_id=text_data_json['sender_id'])
+        time = await self.create_messages(message=text_data_json["message"], sender_id=text_data_json["sender_id"])
 
         await self.channel_layer.group_send(
-            self.room_name, 
+            self.room_name,
             {
                 "type":"chat_message",
                 "message":text_data_json['message'],
@@ -48,7 +43,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             )
         )
 
-    @ database_sync_to_async
+    @database_sync_to_async
     def create_messages(self, message, sender_id):
         chatgroup = ChatGroup.objects.get(name=self.room_name)
         sender = User.objects.get(id=sender_id)
@@ -56,13 +51,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         return message.time.strftime("%h-%d  %H:%M")
 
-
-    async def fetch_history(self, event):
+    async def fetch_history(self):
         messages = await self.history_messages()
         for message in messages:
             await self.send(text_data=json.dumps(message))
-
-
+    
     @database_sync_to_async
     def history_messages(self):
         chatgroup = ChatGroup.objects.get(name=self.room_name)
@@ -71,8 +64,54 @@ class ChatConsumer(AsyncWebsocketConsumer):
         serialized_messages = [
             {
                 "message":msg.body,
-                "sender_id":msg.sender_id,
+                "sender_id":msg.sender.id,
                 "time":msg.time.strftime("%h-%d  %H:%M")
             } for msg in messages
         ]
         return serialized_messages
+    
+
+
+class ProfileListConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_id = self.scope["user"].id
+        self.group_name = "online_users"
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+        await self.update_user_profile("online")
+        await self.channel_layer.group_send(self.group_name, {"type":"users_list"})
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+        await self.update_user_profile(datetime.now().strftime("%H:%M, %d-%m-%Y"))
+        await self.channel_layer.group_send(self.group_name, {"type":"users_list"})
+
+
+    async def receive(self):
+        pass
+
+    async def users_list(self, event):
+        users = await self.all_users()
+        await self.send(text_data=json.dumps(users))
+
+    @database_sync_to_async
+    def all_users(self):
+        profiles = Profile.objects.all()
+
+        serialized_profiles = [
+            {
+                "full_name": str(profile.user.get_full_name()),
+                "status":profile.status,
+                "username":profile.user.username,
+                "picture":profile.picture.url,
+            } for profile in profiles
+        ]
+
+        return serialized_profiles
+    
+    async def update_user_profile(self, status):
+        await self.update_from_database(status)
+
+    @database_sync_to_async
+    def update_from_database(self, status):
+        Profile.objects.filter(user__id=self.user_id).update(status=status)
